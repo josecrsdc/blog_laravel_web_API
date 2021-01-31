4d4d92647437bfe89ef665789bec13cfed16f7fb
Jose <josecarlosrodriguezsanchez89@gmail.com>
2021-01-31 21:28:50 +0100

    api 2.3 ok

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $post = Post::get();
+        return $post;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        // return $post; **Retorna todo el objeto. Mientras que en el array retorna lo que pongas.
+
+        // return [
+        //     'titulo' => $post->titulo,
+        //     'contenido' => $post->contenido
+        // ];
+
+        $post = [
+            'titulo' => $post->titulo,
+            'contenido' => $post->contenido
+        ];
+        // Permite contolar los parametros que retorna y el status.
+        return response()->json($post, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
